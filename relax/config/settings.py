2a770f314e303cee4b5097ce9700439f1166845e
LEONEL <kevinnaguilar001@gmail.com>
2025-10-05 23:25:22 -0300

    qsy

--- relax/config/settings.py
+++ relax/config/settings.py
@@ -21,7 +21,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "|AvF,Vg:|`t#[72DuvS<q9s7EE$@oH")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
